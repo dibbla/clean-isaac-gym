40d57f212567f7ae73df42397bd43bd6b430b85d
dibbla <120090375@link.cuhk.edu.cn>
2023-10-07 18:35:28 +0800

    include IsaacGymEnv from NVIDIA & test code

---
+++ test_env_IsaacGymEnvs.py
@@ -0,0 +1,23 @@
+import isaacgym
+import isaacgymenvs
+import torch
+
+num_envs = 2000
+
+envs = isaacgymenvs.make(
+	seed=0, 
+	task="Ant", 
+	num_envs=num_envs, 
+	sim_device="cuda:0",
+	rl_device="cuda:0",
+	graphics_device_id=0,
+	headless=False,
+	multi_gpu=False,
+	force_render=True,
+)
+print("Observation space is", envs.observation_space)
+print("Action space is", envs.action_space)
+obs = envs.reset()
+for _ in range(2000):
+	random_actions = 2.0 * torch.rand((num_envs,) + envs.action_space.shape, device = 'cuda:0') - 1.0
+	envs.step(random_actions)
